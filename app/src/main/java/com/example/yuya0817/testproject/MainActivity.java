2a0705c24b2cea44b8d4940dd7251cacc594d58c
yuya8083 <yuya.kamito.7n@stu.hosei.ac.jp>
2016-07-31 18:03:38 +0900

    [ADD]kamito

--- app/src/main/java/com/example/yuya0817/testproject/MainActivity.java
+++ app/src/main/java/com/example/yuya0817/testproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yuya0817.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"こんにちは",Toast.LENGTH_LONG).show();
     }
+
 }
