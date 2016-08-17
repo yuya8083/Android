67d1cdb6ef9e052f1b6cde9b9583881345175690
lirei2 <rei.soocer0814@icloud.com>
2016-08-17 13:21:37 +0900

    main2を追加しました

--- app/src/main/java/com/example/yuya0817/testproject/MainActivity.java
+++ app/src/main/java/com/example/yuya0817/testproject/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.yuya0817.testproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this,"こんにちは",Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"PBLだー!",Toast.LENGTH_SHORT).show();
+        Button myButton=(Button)findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
