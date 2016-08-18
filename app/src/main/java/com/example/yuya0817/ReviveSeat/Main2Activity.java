dc5a120f5be8b2eb2f9e9d5bb25c3fecb73926c3
yuya8083 <yuya.kamito.7n@stu.hosei.ac.jp>
2016-08-19 04:04:09 +0900

    テーブル作成画面

--- app/src/main/java/com/example/yuya0817/ReviveSeat/Main2Activity.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/Main2Activity.java
@@ -1,14 +1,34 @@
 package com.example.yuya0817.ReviveSeat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    EditText editText;
+    Button buttoncreat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        editText = (EditText) findViewById(R.id.editText);
+
+        buttoncreat = (Button) findViewById(R.id.creat);
+        buttoncreat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+            }
+        });
+    }
+
+    public void onbackButtontapped(View view){
+        finish();
     }
 }
