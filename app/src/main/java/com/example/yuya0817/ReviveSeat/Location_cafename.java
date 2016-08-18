84968781a476b01ec0b9a080c92582746b3753d0
chiho <test@example.com>
2016-08-19 00:24:05 +0900

    [BANNO];

---
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/Location_cafename.java
@@ -0,0 +1,15 @@
+package com.example.yuya0817.ReviveSeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Location_cafename extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_cafename);
+
+
+    }
+}
--- app/src/main/java/com/example/yuya0817/ReviveSeat/Select_find_method.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/Select_find_method.java
@@ -1,7 +1,10 @@
 package com.example.yuya0817.ReviveSeat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Select_find_method extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class Select_find_method extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_find_method);
+
+        //店舗ボタン→位置情報・店舗名
+        Button tenpo = (Button) findViewById(R.id.cafe);
+        tenpo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_find_method.this, Location_cafename.class);
+                startActivity(intent);
+            }
+        });
+
+        Button categori = (Button) findViewById(R.id.category);
+        categori.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Select_find_method.this, Category_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
