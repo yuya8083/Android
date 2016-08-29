451fe4c31bfdf09c7ed5aada9d8a702c99d1ae13
lirei2 <rei.soocer0814@icloud.com>
2016-08-29 14:39:16 +0900

    歴史の更新

--- app/src/main/java/com/example/yuya0817/ReviveSeat/Main2Activity.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/Main2Activity.java
@@ -65,32 +65,7 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
-        /*Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        String compareValue = "カテゴリ";
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        if (!compareValue.equals(null)) {
-            int spinnerPosition = adapter.getPosition(compareValue);
-            spinner.setSelection(spinnerPosition);
-        }
 
-        //スピナーのクリックイベントを取得する
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            //Itemが選択された時
-            public void onItemSelected(AdapterView parent, View view, int position, long id) {
-                //parentのspinnerを指定
-                Spinner spinner = (Spinner) parent;
-                //選択されたitemを取得
-                String item = (String) spinner.getSelectedItem();
-                //Toast表示
-                Toast.makeText(Main2Activity.this, String.format("%sが選択されました。", item), Toast.LENGTH_SHORT).show();
-            }
-            //何も選択されなかったとき
-            public void onNothingSelected(AdapterView parent) {
-                Toast.makeText(Main2Activity.this, "何も選択されませんでした", Toast.LENGTH_SHORT).show();
-            }
-        });*/
 
         editText = (EditText) findViewById(R.id.biko);
 
