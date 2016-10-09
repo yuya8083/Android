45e620cd34b5ad4b4696b35feb875096750b047d
lirei2 <rei.soocer0814@icloud.com>
2016-10-10 06:14:57 +0900

    画面Fのレイアウト

---
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/matching.java
@@ -0,0 +1,13 @@
+package com.example.yuya0817.ReviveSeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class matching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching);
+    }
+}
--- app/src/main/java/com/example/yuya0817/ReviveSeat/select_share_sheet_host.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/select_share_sheet_host.java
@@ -1,13 +1,62 @@
 package com.example.yuya0817.ReviveSeat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class select_share_sheet_host extends AppCompatActivity {
+
+    String host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_share_sheet_host);
+
+
+
+        Button button1=(Button)findViewById(R.id.button1);
+        button1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                host="R.id.Button1";
+            }
+        }));
+
+        Button button2=(Button)findViewById(R.id.button2);
+        button1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                host="R.id.Button2";
+            }
+        }));
+
+        Button button3=(Button)findViewById(R.id.button3);
+        button1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                host="R.id.Button3";
+            }
+        }));
+
+        Button button4=(Button)findViewById(R.id.button4);
+        button1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                host="R.id.Button4";
+            }
+        }));
+
+        Button next=(Button)findViewById(R.id.next);
+        button1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(select_share_sheet_host.this, select_share_sheet_guest.class);
+                intent.putExtra("host",host);
+                startActivity(intent);
+            }
+        }));
     }
 }
