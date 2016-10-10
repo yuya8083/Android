9eb4a3c05b8b28a3bf43b99abb5dbd9de7ad4a76
lirei2 <rei.soocer0814@icloud.com>
2016-10-11 06:00:18 +0900

    画面A補足

---
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/chat_system.java
@@ -0,0 +1,15 @@
+package com.example.yuya0817.ReviveSeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chat_system extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_system);
+
+
+    }
+}
--- app/src/main/java/com/example/yuya0817/ReviveSeat/share_table_list.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/share_table_list.java
@@ -1,7 +1,10 @@
 package com.example.yuya0817.ReviveSeat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class share_table_list extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class share_table_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_table_list);
+
+        ImageButton back=(ImageButton)findViewById(R.id.back);
+        back.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(share_table_list.this, MainActivity.class);
+                startActivity(intent1);
+            }
+            }));
     }
 }
