7854890fa3958ddab6def14c0dc89df000fca9c8
chiho <test@example.com>
2016-10-11 15:08:52 +0900

    check_in

---
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/check_in.java
@@ -0,0 +1,16 @@
+package com.example.yuya0817.ReviveSeat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class check_in extends Activity {
+
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_in);
+
+    }
+}
