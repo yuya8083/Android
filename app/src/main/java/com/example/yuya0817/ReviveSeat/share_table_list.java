e22d34d88d69bd6341010c7ad8e869f9ce0f226a
lirei2 <rei.soocer0814@icloud.com>
2016-10-17 16:09:45 +0900

    つなぎ2

--- app/src/main/java/com/example/yuya0817/ReviveSeat/share_table_list.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/share_table_list.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class share_table_list extends AppCompatActivity {
@@ -17,9 +18,54 @@ public class share_table_list extends AppCompatActivity {
         back.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(share_table_list.this, MainActivity.class);
-                startActivity(intent1);
+                Intent intent= new Intent(share_table_list.this, MainActivity.class);
+                startActivity(intent);
             }
             }));
+
+        Button list1=(Button)findViewById(R.id.list1);
+        list1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(share_table_list.this, JoinConfirmation.class);
+                startActivity(intent1);
+            }
+        });
+
+        Button list2=(Button)findViewById(R.id.list2);
+        list2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(share_table_list.this, JoinConfirmation.class);
+                startActivity(intent2);
+            }
+        });
+
+        Button list3=(Button)findViewById(R.id.list3);
+        list3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(share_table_list.this, JoinConfirmation.class);
+                startActivity(intent3);
+            }
+        });
+
+        Button list4=(Button)findViewById(R.id.list4);
+        list4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent(share_table_list.this, JoinConfirmation.class);
+                startActivity(intent4);
+            }
+        });
+
+        Button list5=(Button)findViewById(R.id.list5);
+        list5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent5 = new Intent(share_table_list.this, JoinConfirmation.class);
+                startActivity(intent5);
+            }
+        });
     }
 }
