3d46152a97d096449bc576da93cc84af82bf9b61
lirei2 <rei.soocer0814@icloud.com>
2016-10-19 13:39:11 +0900

    a

--- app/src/main/java/com/example/yuya0817/ReviveSeat/matching.java
+++ app/src/main/java/com/example/yuya0817/ReviveSeat/matching.java
@@ -24,7 +24,7 @@ public class matching extends Activity {
             }
         });
         Button share=(Button)findViewById(R.id.share);
-        cancel.setOnClickListener(new View.OnClickListener() {
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(matching.this, wait.class);
